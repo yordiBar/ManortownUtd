a239588bc62e1a2dab1835bb33c28509589d8ab7
Bart <bart.rybak@hotmail.com>
2020-05-22 18:28:40 +0100

    order charts added plus some tests

---
+++ tests/UsernameTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\tests;
+
+use App\Entity\User;
+use \PHPUnit\Framework\TestCase;
+
+class UsernameTest extends TestCase
+{
+    public function getUsernameTest()
+    {
+        $username = new User();
+
+        $username->setUsername('admin');
+
+        $this->assertEquals('admin', $username->getUsername());
+    }
+
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
